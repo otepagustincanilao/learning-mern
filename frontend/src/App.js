bfd3857047de3152d5c76dec95b7104fb3253a3e
joseph canilao <joseph_canilao@medilink.com.ph>
2021-02-14 17:05:52 +0800

    done for ratings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import Footer from './components/Footer'
+
 
 const App = () => {
   return (
     <>
-      <h1> Welcome To ProShop</h1>
+      <Header />
+
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      
+      <Footer />
     </>
   );
 }
