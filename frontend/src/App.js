dfb8f2d13b3f96ff16e103a997bb0402d4f9a1ce
joseph canilao <agustin.canilao@gmail.com>
2021-02-16 00:04:02 +0800

    done for 3.2 Serving Products - Back End Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
-import HomeScreen from './screens/HomeScreen'
 import Footer from './components/Footer'
-
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
 
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact /> 
+          <Route path='/product/:id' component={ProductScreen} /> 
         </Container>
       </main>
       
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
