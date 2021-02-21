5ed2da261239c080f347b886e12f2bb725edb95e
joseph canilao <agustin.canilao@gmail.com>
2021-02-21 23:20:24 +0800

    4.5 add user data for seeding and use bcryptjs as password dependency

---
+++ backend/data/users.js
@@ -0,0 +1,39 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('P@ssw0rd', 10),
+        isAdmin: true
+    },
+    {
+        name: 'Cj Francisco',
+        email: 'cj_francisco@gmail.com',
+        password: bcrypt.hashSync('P@ssw0rdcj', 10),
+        isAdmin: false
+    },
+    {
+        name: 'Anton Santiago',
+        email: 'anton_santiago@gmail.com',
+        password: bcrypt.hashSync('P@ssw0rdanton', 10),
+        isAdmin: false
+    },
+    {
+        name: 'Shella Magallano',
+        email: 'shella_magallano@gmail.com',
+        password: bcrypt.hashSync('P@ssw0rdshella', 10),
+        isAdmin: false
+    },
+    {
+        name: 'Raymond Navarro',
+        email: 'raymond_navarro@gmail.com',
+        password: bcrypt.hashSync('P@ssw0rdraymond', 10),
+        isAdmin: false
+    }
+
+  ]
+  
+  export default users         // ## this is ES modules
+  // module.exports = products    // ## common js
+  
