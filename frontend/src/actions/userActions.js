deb5593af4ad23ed1c3be5801b518ce837916394
joseph canilao <agustin.canilao@gmail.com>
2021-03-31 22:25:15 +0800

    8.3 Show User In header Navbar & header Logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants"
+import { 
+    USER_LOGIN_FAIL, 
+    USER_LOGIN_REQUEST, 
+    USER_LOGIN_SUCCESS, 
+    USER_LOGOUT 
+} from "../constants/userConstants"
 
 export const login = (email, password) => async (dispatch) => {
     try{
@@ -35,4 +40,9 @@ export const login = (email, password) => async (dispatch) => {
                 : error.message,
         })
     }
+}
+
+export const logout = () => async(dispatch) => {
+    localStorage.removeItem('userInfo')
+    dispatch({ type: USER_LOGOUT })
 }
