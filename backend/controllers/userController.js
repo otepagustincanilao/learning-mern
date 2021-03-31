211fcaeb842af49b26b0565c80bf76c0ab460487
joseph canilao <agustin.canilao@gmail.com>
2021-03-31 18:06:37 +0800

    done for backend user authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,6 +27,66 @@ const authUser = asyncHandler(async(req, res) => {
     }
 })
 
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public  
+const registerUser = asyncHandler(async(req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if(userExists){
+        res.status(400) // badrequest
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201) // 201 means something is created
+        .json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)    
+        })
+    }
+    else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
+// @desc    Get User Profile
+// @route   POST /api/users/profile
+// @access  Private  
+const getUserProfile = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user){
+        res.json(
+            {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin               
+            }
+        )
+    }
+    else{
+        console.log(2)
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 export {
-    authUser
+    authUser,
+    registerUser,
+    getUserProfile
 }
