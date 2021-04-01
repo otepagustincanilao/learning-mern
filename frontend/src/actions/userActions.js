6baee39baa1cef573ee589cf79bb0a480166e87f
joseph canilao <agustin.canilao@gmail.com>
2021-04-01 22:00:29 +0800

    done 8.6 Profile Screen & Get User Details

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import { 
+	USER_DETAILS_FAIL,
+	USER_DETAILS_REQUEST,
+	USER_DETAILS_SUCCESS,
     USER_LOGIN_FAIL, 
     USER_LOGIN_REQUEST, 
     USER_LOGIN_SUCCESS, 
@@ -12,8 +15,8 @@ import {
 export const login = (email, password) => async (dispatch) => {
     try{
         dispatch({
-            type: USER_LOGIN_REQUEST
-        })
+					type: USER_LOGIN_REQUEST
+				})
 
         const config = {
             headers: {
@@ -89,4 +92,40 @@ export const register = (name, email, password) => async (dispatch) => {
                 : error.message,
         })
     }
+}
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+	try{
+			dispatch({
+					type: USER_DETAILS_REQUEST
+			})
+
+			const { userLogin: { userInfo } } = getState()
+
+			const config = {
+					headers: {
+							'Content-Type': 'application/json',
+							Authorization: `Bearer ${userInfo.token}`
+					}
+			}
+
+			const { data } = await axios.get(
+					`/api/users/${id}`, 
+					config
+			)
+
+			dispatch({
+					type: USER_DETAILS_SUCCESS,
+					payload: data
+			})
+	}
+	catch(error){
+			dispatch({
+					type: USER_DETAILS_FAIL,
+					payload:
+						error.response && error.response.data.message
+							? error.response.data.message
+							: error.message,
+			})
+	}
 }
