ff2f7873fa6d0d7e6c7510f297c99f7c5f97bf40
joseph canilao <agustin.canilao@gmail.com>
2021-04-08 00:56:41 +0800

    done 8.2 Checkout Steps Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
 
 const App = () => {
   return (
@@ -17,8 +18,9 @@ const App = () => {
 
       <main className="py-3">
         <Container>
+          <Route path='/shipping' component={ShippingScreen} />
           <Route path='/login' component={LoginScreen} /> 
-          <Route path='/register' component={RegisterScreen} /> 
+          <Route path='/register' component={RegisterScreen} />           
           <Route path='/profile' component={ProfileScreen} /> 
           <Route path='/product/:id' component={ProductScreen} /> 
           <Route path='/cart/:id?' component={CartScreen} /> 
--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,36 +2,46 @@ import axios from 'axios'
 
 import { 
     CART_ADD_ITEM,
-    CART_REMOVE_ITEM
+    CART_REMOVE_ITEM,
+	CART_SAVE_SHIPPING_ADDRESS
  } from '../constants/cartConstants'
 
- export const addToCart = (id, qty) => async(dispatch, getState) => {
-    console.log("cart_action")
-    const { data } = await axios.get(`/api/products/${id}`)
+export const addToCart = (id, qty) => async(dispatch, getState) => {
+	console.log("cart_action")
+	const { data } = await axios.get(`/api/products/${id}`)
 
-    //dispatch updating process muna before proceed to localStorage update using getState()
-    dispatch({
-        type: CART_ADD_ITEM,
-        payload: {
-            product: data._id,
-            name: data.name,
-            image: data.image,
-            price: data.price,
-            countInStock: data.countInStock,
-            qty: qty
-        }
-    })
-    
-    // console.log(getState().cart.cartItems)
-    // we use stringify bec we can only use json string to localstorage
-    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
- }
+	//dispatch updating process muna before proceed to localStorage update using getState()
+	dispatch({
+		type: CART_ADD_ITEM,
+		payload: {
+				product: data._id,
+				name: data.name,
+				image: data.image,
+				price: data.price,
+				countInStock: data.countInStock,
+				qty: qty
+		}
+	})
+	
+	// console.log(getState().cart.cartItems)
+	// we use stringify bec we can only use json string to localstorage
+	localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
 
- export const removeFromCart = (id) => async(dispatch, getState) => {
-    dispatch({
-        type: CART_REMOVE_ITEM,
-        payload: id
-    })
+export const removeFromCart = (id) => async(dispatch, getState) => {
+	dispatch({
+		type: CART_REMOVE_ITEM,
+		payload: id
+	})
 
-    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
- }
+	localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const saveShippingAddress = (data) => async(dispatch) => {
+	dispatch({
+		type: CART_SAVE_SHIPPING_ADDRESS,
+		payload: data
+	})
+
+  localStorage.setItem('shippingAddress', JSON.stringify(data))
+}
